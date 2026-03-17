ea124b38a697a47594a6455787a71bb1976e12bf
Russell Bryan Tan <greentan31@gmail.com>
2026-03-17 12:11:01 +0800

    Add name length validation (6-10 characters) to Cafe and Employee entities

--- backend/src/CafeEmployeeManager.Domain/Entities/Cafe.cs
+++ backend/src/CafeEmployeeManager.Domain/Entities/Cafe.cs
@@ -1,8 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace CafeEmployeeManager.Domain.Entities;
 
 public class Cafe
 {
     public Guid Id { get; set; }
+    [StringLength(10, MinimumLength = 6, ErrorMessage = "Cafe name must be between 6 and 10 characters")]
     public string Name { get; set; } = default!;
     public string Description { get; set; } = default!;
     public string? Logo { get; set; }
--- backend/src/CafeEmployeeManager.Domain/Entities/Employee.cs
+++ backend/src/CafeEmployeeManager.Domain/Entities/Employee.cs
@@ -1,8 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace CafeEmployeeManager.Domain.Entities;
 
 public class Employee
 {
     public string Id { get; set; } = default!; // UIXXXXXXX
+    [StringLength(10, MinimumLength = 6, ErrorMessage = "Employee name must be between 6 and 10 characters")]
     public string Name { get; set; } = default!;
     public string EmailAddress { get; set; } = default!;
     public string PhoneNumber { get; set; } = default!;
